273971df8e6207cb97d6c65c18ef8c26513782ba
rashmi-shree.github.io <rashmi.s0297@gmail.com>
2020-10-12 20:30:02 +0530

    contact form finished

--- assets/js/main1.js
+++ assets/js/main1.js
@@ -2,9 +2,11 @@ window.onload=function(){
 	var name = document.getElementById("name")
 	var send_message = document.getElementsByClassName("send_message_button")[0]
  	
-
+    
  	send_message.addEventListener("click", function(){
-	console.log(name.value);
+	
+		document.getElementById('msg').style.display = "block";
+	
 	})
 
  }
